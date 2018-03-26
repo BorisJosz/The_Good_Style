e89154692bb31fe2e59675b91963bc0cb8cdb241
Christophe van Cutsem <christophevc91@gmail.com>
2018-03-26 16:37:20 +0200

    created models for products brands and users

--- db/migrate/20180326134842_devise_create_users.rb
+++ db/migrate/20180326134842_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :telephone
 
       ## Recoverable
       t.string   :reset_password_token
