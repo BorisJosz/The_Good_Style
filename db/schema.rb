a9b7589fe87e7954c9638af45d4296e862090203
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-26 17:10:00 +0200

    add product controller

--- db/schema.rb
+++ db/schema.rb
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(version: 20180326142850) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.string "address", null: false
-    t.string "telephone"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
