780bcad3d773040a454e88f04dd3c1e4dbce5980
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-27 13:46:05 +0200

    set meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
+
+def default_url_options
+  { host: ENV["HOST"] || "www.thegoodstyle.store" }
+end
+
 end
