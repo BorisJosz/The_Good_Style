89b60f68ae1940a57b08b98d6bd8cd631267bbc4
Aline Sinzo <alinesinzo@yahoo.fr>
2018-03-27 13:56:17 +0200

    details model product

--- app/models/target_audience.rb
+++ app/models/target_audience.rb
@@ -1,3 +1,5 @@
 class TargetAudience < ApplicationRecord
   belongs_to :product
+  validates :gender, presence:true, inclusion: in {("Men", "Women", "Kids")}
+  validates :target_photo, presence:true
 end
