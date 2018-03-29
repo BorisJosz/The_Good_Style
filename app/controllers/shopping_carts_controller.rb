4e6fbfbf268f5c3cc809a96c3140393d34d7346a
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-29 15:45:11 +0200

    add to cart

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -9,17 +9,22 @@ class ShoppingCartsController < ApplicationController
     # 1 Find Item from Route info
    @product = Product.find(params[:product_id])
    # 2 Get from post params info about product variation to identify what are the specificities of desired item
-   @productVariation = ProductVariation.where(product: @product, size_id: set_variations[:size], color_id: set_variations[:color])
+   @productVariation = ProductVariation.where(product: @product, size_id: set_variations[:size].to_i, color_id: set_variations[:color].to_i )
    # 3 Establish wither we have a shopping cart
-   @shoppingCart = ShoppingCart.where(user: current_user, status: false)
-   if @shoppingCart.empty?
+   @shoppingCart = ShoppingCart.where(user: current_user, status: false).first
+   if @shoppingCart.nil?
     # 4 if we don't have a shopping cart, we create one
     @shoppingCart = ShoppingCart.create(user: current_user, status: false)
    end
      # 5 as we have all the infos to create a shopping cart item in DB, we create it
-    ShoppingCartItem.create!(product_variation: @productVariation.first, shopping_cart: @shopping_cart)
+    ShoppingCartItem.create!(product_variation: @productVariation.first, shopping_cart: @shoppingCart)
     # 6 render js code so we dont change page and can yet update
-    format.js
+    redirect_to product_path(@product)
+
+  end
+
+  def quantity
+
   end
 
   def create
