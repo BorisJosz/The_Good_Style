3f66cf8408b860ae305ade03f2c4d29f65fe78e0
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-30 16:25:20 +0200

    thank you page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,25 +9,24 @@ class PaymentsController < ApplicationController
     source: params[:stripeToken],
     email:  params[:stripeEmail]
   )
-  @shopping_cart.amount_cents = @shopping_cart.total
+  amount_cents = @shopping_cart.total.to_i
   charge = Stripe::Charge.create(
     customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @shopping_cart.amount_cents,
+    amount:       amount_cents,
     description:  "Payment for THE GOOD STYLE for order #{@shopping_cart.id}",
     currency:     @shopping_cart.amount.currency
   )
 
-  @shopping_cart.update(payment: charge.to_json, state: 'paid')
-  redirect_to shopping_cart_path(@shopping_cart)
+  @shopping_cart.update(payment: charge.to_json, status: 'paid')
+  redirect_to shopping_cart_thanks_path(@shopping_cart)
 
 rescue Stripe::CardError => e
   flash[:alert] = e.message
-  redirect_to new_shopping_cart_payment_path(@shopping_cart)
+  redirect_to shopping_cart_thanks_path(@shopping_cart)
 end
-
 private
 
   def set_order
-    @shopping_cart = ShoppingCart.where(status: false).find(params[:shopping_cart_id])
+    @shopping_cart = ShoppingCart.find(params[:shopping_cart_id])
   end
 end
