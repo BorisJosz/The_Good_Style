8bf87effaee2e8d129b12fce32db8ababf199257
Boris Josz <Boris.Josz@gmail.com>
2018-03-30 14:18:32 +0200

    navbar fix, search implementation, shopping cart link in navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,14 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @brand = @product.brand
   end
+
+  def index
+    if params[:query].present?
+      @products = Product.where(name: params[:query])
+    else
+      @products = Product.all
+    end
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :products, only: [:show] do
+  resources :products, only: [:index, :show] do
     post "add_item", to: "shopping_carts#add_item"
   end
 
