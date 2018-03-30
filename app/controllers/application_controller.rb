1bc042ec0d62199e72068c4635d5215ab82a9256
Boris Josz <Boris.Josz@gmail.com>
2018-03-30 10:30:17 +0200

    photouploader/added form inputs/linked image to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["HOST"] || "www.thegoodstyle.store" }
+  end
 
-def default_url_options
-  { host: ENV["HOST"] || "www.thegoodstyle.store" }
-end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :address, :photo, :photo_cache])
+  end
 
 end
