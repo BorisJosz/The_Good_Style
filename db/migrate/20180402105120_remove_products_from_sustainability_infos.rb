07da7dd6a2b1b9381888b1a13b3d75abf8e9f6ab
Meg Vandezande <meg.vandezande@gmail.com>
2018-04-02 18:49:49 +0200

    ejropwr weSR

--- db/migrate/20180402105120_remove_products_from_sustainability_infos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductsFromSustainabilityInfos < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :sustainability_infos, :products_id
-  end
-end
--- db/migrate/20180402105738_add_product_to_sustainability_infos.rb
+++
@@ -1,5 +0,0 @@
-class AddProductToSustainabilityInfos < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :sustainability_infos, :product, index: true
-  end
-end
