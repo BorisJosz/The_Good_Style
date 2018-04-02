96c21a50220047dc64591c527be9f0576febce94
Meg Vandezande <meg.vandezande@gmail.com>
2018-04-02 18:30:56 +0200

    m

--- db/migrate/20180402102913_drop_sustainability_info.rb
+++
@@ -1,9 +0,0 @@
-class DropSustainabilityInfo < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :sustainability_infos
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
