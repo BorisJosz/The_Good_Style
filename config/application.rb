6411ad6f7b63babf78a7af307f37defcacd437ec
Boris Josz <Boris.Josz@gmail.com>
2018-04-02 17:27:27 +0200

    shopping-cart-items count implemented

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module TheGoodStyle
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #AJAX
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
