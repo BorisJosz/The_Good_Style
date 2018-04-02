66903b4bc7145623728d2a10a2904e9204301560
Meg Vandezande <meg.vandezande@gmail.com>
2018-04-02 18:10:47 +0200

    meg details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402105827) do
+ActiveRecord::Schema.define(version: 20180402134417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,8 @@ ActiveRecord::Schema.define(version: 20180402105827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
+    t.string "icon"
+    t.string "icon_pale"
     t.index ["product_id"], name: "index_sustainability_infos_on_product_id"
   end
 
