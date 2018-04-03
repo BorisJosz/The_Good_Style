9e9d22c98d80683d11e61f5406018e1de4ee529f
Aline Sinzo <alinesinzo@yahoo.fr>
2018-04-03 10:09:57 +0200

    ignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -329,7 +329,7 @@ SustainabilityInfo.create!(
   rating: 5,
   icon: 'http://res.cloudinary.com/megednazednav/image/upload/v1522679976/thread.svg',
   icon_pale: 'http://res.cloudinary.com/megednazednav/image/upload/v1522679976/thread_1.svg',
-  products_id: Product.where(name:"Colorful dress").first.id,
+  product: Product.where(name:"Colorful dress").first,
   })
 
 SustainabilityInfo.create!(
@@ -339,7 +339,7 @@ SustainabilityInfo.create!(
   rating: 4,
   icon: 'http://res.cloudinary.com/megednazednav/image/upload/v1522679976/happy-man.svg',
   icon_pale: 'http://res.cloudinary.com/megednazednav/image/upload/v1522679976/happy-man_1.svg',
-  products_id: Product.where(name:"Colorful dress").first,
+  product: Product.where(name:"Colorful dress").first,
   })
 
 SustainabilityInfo.create!(
